295aba2d2416ccb8d048c30f936a2485b68f4dae
matheusman <pereiralimamatheuspl@gmail.com>
2024-01-24 16:28:16 -0300

    dotnet core mvc cofig ambiente

---
+++ README.md
@@ -0,0 +1,4 @@
+### Started dotnet core mvc
+
+command run dotnet core:
+ __dotnet new mvc__
--- obj/Debug/net8.0/Course_Aspnetcore_mvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/Course_Aspnetcore_mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Course_Aspnetcore_mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c02836ed207fc8fbff03ce57d81e9ab0af7cd8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Course_Aspnetcore_mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Course_Aspnetcore_mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
